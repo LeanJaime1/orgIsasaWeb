18367de1a382e9b9765efec73664d256e1534ba2
Leandro Jaime <leandrojaime17@gmail.com>
2025-07-24 20:47:41 -0300

    negritas

--- src/assets/components/Us/Us.jsx
+++ src/assets/components/Us/Us.jsx
@@ -30,14 +30,14 @@ const Us = () => {
       <div className="container">
         <h2>Sobre Nosotros</h2>
         <p>
-          Somos una organización de seguros con más de 15 años de trayectoria brindando soluciones integrales a productores en todo el país. Nuestro equipo está conformado por profesionales con más de 30 años de experiencia en el sector, que han trabajado en compañías líderes y conocen en profundidad las dinámicas del mercado asegurador.
+          Somos una <strong>organización de seguros</strong> con más de 15 años de trayectoria brindando soluciones integrales a productores en todo el país. Nuestro equipo está conformado por profesionales con más de 30 años de experiencia en el sector, que han trabajado en compañías líderes y conocen en profundidad las dinámicas del mercado asegurador.
         </p>
         <p>
           Esta base sólida nos permite ofrecer un servicio cercano, eficiente y adaptado a las necesidades reales de quienes producen seguros. 
           Entendemos el negocio desde adentro y acompañamos a cada productor con herramientas comerciales, asesoramiento estratégico y una estructura pensada para facilitar el crecimiento sostenido.
         </p>
         <p className='hide-mobile'>
-          En un mercado que exige agilidad y respaldo, somos un aliado confiable para que te enfoques en lo que mejor sabés hacer: vender y asesorar con confianza.
+          En un mercado que exige agilidad y respaldo, <strong>somos un aliado confiable para que te enfoques en lo que mejor sabés hacer: vender y asesorar con confianza.</strong> 
         </p>
       </div>
     </motion.section>
